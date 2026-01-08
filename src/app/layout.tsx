25b891a132e1053d5cc6534c0b640ef74bef6d24
loandibaxhuku-source <loandibaxhuku@gmail.com>
2026-01-08 18:29:38 +0100

    Set HTML lang attribute to German (de)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="de" suppressHydrationWarning>
       <body>
         <LanguageProvider>
           <div className="flex flex-col min-h-screen bg-white">
